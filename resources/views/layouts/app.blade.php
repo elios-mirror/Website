477dde88a66e99e0c52c8f1824f5bd2dcb73d821
Frost <matthias.prost@epitech.eu>
2018-03-14 17:52:46 +0100

    Minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description"
+          content="Elios est un miroir connecté doté d’une intelligence artificielle il offre une interaction poussée et une personnalisation presque infinie.">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
